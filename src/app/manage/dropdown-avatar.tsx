5ca3476f36a094586839ec9d60bc78a0215c6e2c
felixxplor <nhtu.sgs@gmail.com>
2024-09-22 22:56:55 +1000

    Code UI for dashboard

---
+++ src/app/manage/dropdown-avatar.tsx
@@ -0,0 +1,42 @@
+'use client'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+const account = {
+  name: 'ABC',
+  avatar: 'https://i.pravatar.cc/150',
+}
+export default function DropdownAvatar() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon" className="overflow-hidden rounded-full">
+          <Avatar>
+            <AvatarImage src={account.avatar ?? undefined} alt={account.name} />
+            <AvatarFallback>{account.name.slice(0, 2).toUpperCase()}</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>{account.name}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href={'/manage/setting'} className="cursor-pointer">
+            Settings
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>Support</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Logout</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
