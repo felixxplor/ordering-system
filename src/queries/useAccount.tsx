52d9218c9756172ba316c5fa9aebd6a5c8eac552
felixxplor <nhtu.sgs@gmail.com>
2024-09-22 23:19:01 +1000

    Code get account info

---
+++ src/queries/useAccount.tsx
@@ -0,0 +1,8 @@
+import accountApiRequest from '@/apiRequests/account'
+import { useQuery } from '@tanstack/react-query'
+export const useAccountProfile = () => {
+  return useQuery({
+    queryKey: ['account-profile'],
+    queryFn: accountApiRequest.me,
+  })
+}
