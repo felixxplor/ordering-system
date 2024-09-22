4eb909266f7cc05574b1c3c2b52f90b82ef05ece
felixxplor <nhtu.sgs@gmail.com>
2024-09-22 11:20:20 +1000

    Create a config file to validate environment variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import Image from 'next/image'
+import ClientComponent from './ClientComponent'
 
 export default function Home() {
   return (
@@ -12,9 +13,10 @@ export default function Home() {
           height={38}
           priority
         />
+        <ClientComponent />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Get started by editing{' '}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
@@ -97,5 +99,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
