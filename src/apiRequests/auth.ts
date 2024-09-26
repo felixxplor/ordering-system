0a1be0dd7e1eb4d3625addf0d4c65f8cdf8d0922
felixxplor <nhtu.sgs@gmail.com>
2024-09-26 23:22:22 +1000

    Create refresh token route handler

--- src/apiRequests/auth.ts
+++ src/apiRequests/auth.ts
@@ -1,5 +1,11 @@
 import http from '@/lib/http'
-import { LoginBodyType, LoginResType, LogoutBodyType } from '@/schemaValidations/auth.schema'
+import {
+  LoginBodyType,
+  LoginResType,
+  LogoutBodyType,
+  RefreshTokenBodyType,
+  RefreshTokenResType,
+} from '@/schemaValidations/auth.schema'
 
 const authApiRequest = {
   sLogin: (body: LoginBodyType) => http.post<LoginResType>('/auth/login', body),
@@ -24,5 +30,11 @@ const authApiRequest = {
       }
     ),
   logout: () => http.post('/api/auth/logout', null, { baseUrl: '' }), // The client calls the route handler, and there's no need to pass the Access Token and Refresh Token in the body because the AT and RT are automatically sent through cookies.
+  sRefreshToken: (body: RefreshTokenBodyType) =>
+    http.post<RefreshTokenResType>('/auth/refresh-token', body),
+  refreshToken: () =>
+    http.post<RefreshTokenResType>('/api/auth/refresh-token', null, {
+      baseUrl: '',
+    }),
 }
 export default authApiRequest
